fac918452412ec12dc067182e042b67e908c6313
Bea <burbuqe.shaqiri98@gmail.com>
2026-04-15 21:31:32 +0200

    Prepare backend settings for deployment

--- backend/ar_re_backend/settings.py
+++ backend/ar_re_backend/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.contrib.auth import get_user_model
 
 load_dotenv()
 
@@ -125,3 +126,16 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+if os.environ.get("DJANGO_SUPERUSER_USERNAME"):
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+        )
